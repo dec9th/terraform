e45682996ccccfbb48a21a7449574dfbb91f8058
dec9th <33111226+dec9th@users.noreply.github.com>
2022-01-21 03:44:48 +0900

    Update var.tf
    add transfer_ssh_key_SANTA

--- sample/sftp/var.tf
+++ sample/sftp/var.tf
@@ -9,3 +9,15 @@ variable "subnet_ids" {
   default     = ["subnet-aa548cc3", "subnet-11718a6a","subnet-c2637888"]
 }
 
+variable "transfer_ssh_key_SANTA" {
+  description = "type SANTA's ssh_key"
+  sensitive   = true
+  default     = "ssh-key already typed. # if you replace this, remove default"
+  
+  validation {
+    # regex(...) fails if it cannot find a match
+    condition     = can(regex("^ssh-", var.transfer_ssh_key_SANTA))
+    error_message = "The transfer_ssh_key_SANTA value must be a valid ssh key, starting with \"ssh-\"."
+  }
+  
+}
